2599e7e82cf682757885a81ef3c615f588fe2629
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-03 21:18:55 +0400

    should work now

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <imgui_internal.h>
+#include <imgui.h>
+#include <GLFW/glfw3.h>
 
 int main() {
   std::printf("are we working?");
+
+  bool isRunning; 
+
+  ImGui::Begin("imlittlesoundboard", &isRunning, ImGuiWindowFlags_MenuBar);
   return 0;
 }
