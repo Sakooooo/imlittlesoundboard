37a64551a56feb7cff890c9d26d9e064a62066b4
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-03 21:54:49 +0400

    ?????????????????????????????????

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
-#include <GLFW/glfw3.h>
+#include <imgui_impl_opengl3.h>
 #include <GL/GL.h>
-
-int makeImGuiWindow(GLFWwindow* window) {
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
-  ImGui::StyleColorsDark();
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  return 0;
-}
+#include <GLFW/glfw3.h>
 
 int main() {
 
@@ -25,28 +17,34 @@ int main() {
 
   glfwMakeContextCurrent(window);
 
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  ImGui::StyleColorsDark();
+
   while (!glfwWindowShouldClose(window))
     {
       float ratio;
       int width, height;
       glfwGetFramebufferSize(window, &width, &height);
-      ratio = width / (float)height;
-      glViewport(0, 0, width, height);
-      glClear(GL_COLOR_BUFFER_BIT);
-      glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-      glMatrixMode(GL_MODELVIEW);
-      glLoadIdentity();
-      glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-      glBegin(GL_TRIANGLES);
-      glColor3f(1.f, 0.f, 0.f);
-      glVertex3f(-0.6f, -0.4f, 0.f);
-      glColor3f(0.f, 1.f, 0.f);
-      glVertex3f(0.6f, -0.4f, 0.f);
-      glColor3f(0.f, 0.f, 1.f);
-      glVertex3f(0.f, 0.6f, 0.f);
-      glEnd();
+
+      // do stuff here
+
+      // setup imgui frame
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+
+      // begin imgui thing
+      ImGui::Begin("ImGUI Little Soundboard");
+      ImGui::Text("Im not supposed to be here...");
+      ImGui::End();
+
+      // push to context
+      ImGui::Render();
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
       glfwSwapBuffers(window);
       glfwPollEvents();
     };
