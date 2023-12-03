0b59af4ec4203366a62df845d3c7ef5e8f21dad3
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-03 21:42:11 +0400

    lol look a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,57 @@
 #include <iostream>
-#include <imgui_internal.h>
 #include <imgui.h>
+#include <imgui_impl_glfw.h>
 #include <GLFW/glfw3.h>
+#include <GL/GL.h>
+
+int makeImGuiWindow(GLFWwindow* window) {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  ImGui::StyleColorsDark();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  return 0;
+}
 
 int main() {
-  std::printf("are we working?");
 
-  bool isRunning; 
+  if (!glfwInit())
+    return 1;
 
-  ImGui::Begin("imlittlesoundboard", &isRunning, ImGuiWindowFlags_MenuBar);
+  GLFWwindow* window = glfwCreateWindow(800, 800, "imlittlesoundboard", NULL, NULL); 
+
+  if (!window)
+    return 1;
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window))
+    {
+      float ratio;
+      int width, height;
+      glfwGetFramebufferSize(window, &width, &height);
+      ratio = width / (float)height;
+      glViewport(0, 0, width, height);
+      glClear(GL_COLOR_BUFFER_BIT);
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+      glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+      glBegin(GL_TRIANGLES);
+      glColor3f(1.f, 0.f, 0.f);
+      glVertex3f(-0.6f, -0.4f, 0.f);
+      glColor3f(0.f, 1.f, 0.f);
+      glVertex3f(0.6f, -0.4f, 0.f);
+      glColor3f(0.f, 0.f, 1.f);
+      glVertex3f(0.f, 0.6f, 0.f);
+      glEnd();
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    };
+
+  glfwTerminate();
+
   return 0;
 }
